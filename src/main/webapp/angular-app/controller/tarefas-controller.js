f355cfd87e00338aef40656f89a0be5c871e65c5
Arthur Garcia <arthur.ttco@hotmail.com>
2017-04-02 16:02:58 -0300

    implementando serviços angular.

--- src/main/webapp/angular-app/controller/tarefas-controller.js
+++ src/main/webapp/angular-app/controller/tarefas-controller.js
@@ -3,7 +3,21 @@
  */
 app.controller('TarefasController', ['$scope', 
       'tarefas-service', function($scope, tarefasService) {
-		tarefasService.success(function(data) {
+		
+	tarefasService.list().success(function(data) {
 		    $scope.tarefas = data;
+	});
+	
+	$scope.tarefaNova = {
+	        titulo: '',
+	        descricao: ''
+	};
+	
+	$scope.salvar = function(form) { 
+
+		tarefasService.save($scope.tarefaNova.titulo, $scope.tarefaNova.descricao).success(function() {
+		    
 		});
+    };
+	
 }]);
--- src/main/webapp/angular-app/services/tarefas-service.js
+++ src/main/webapp/angular-app/services/tarefas-service.js
@@ -2,12 +2,35 @@
  * 
  */
 app.factory('tarefas-service', ['$http', function($http) { 
+	
+	var tarefasService = {
+			list: function(){
+				return $http.get('http://localhost:8080/tasklist/rest/tarefas') 
+				.success(function(data) { 
+        			return data; 
+        		}) 
+        		.error(function(err) { 
+        			return err; 
+        		});
+			},
+            		
+            save: function(titulo, descricao){
+            	$http.post( 'http://localhost:8080/tasklist/rest/tarefas', 
+                        null, 
+                        {"content-type":'application/json',
+                         params: {
+                                  "titulo": titulo,
+                                  "descricao": descricao}
+                          })
+                          .success(function() { 
+                  			console.log("ok");
+                  		}) 
+                  		.error(function(err) { 
+                  			console.log("erro");
+                  		});
+            }
+            
+	};
   
-	return $http.get('http://localhost:8080/tasklist/rest/tarefas') 
-            .success(function(data) { 
-              return data; 
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
+	return tarefasService;
 }]);
